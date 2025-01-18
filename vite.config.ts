7ec2fcb55fe5602e65f7f6b1c4cfdf4372d026c1
HEmant Govekar <hemantgovekar@gmail.com>
2025-01-18 09:30:38 -0500

    package file changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/speech-to-text/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
